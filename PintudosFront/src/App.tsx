d5ab93d1474ffac5b1d22948878394e2299f6296
Juan <112003348+juanescan@users.noreply.github.com>
2025-03-30 21:37:20 -0500

    mejoras en la pagina inicial

--- PintudosFront/src/App.tsx
+++ PintudosFront/src/App.tsx
@@ -6,12 +6,10 @@ function App() {
     <div className="flex items-center justify-center min-h-screen bg-purple-600">
       <div className="bg-white p-8 rounded-xl shadow-lg w-96 text-center">
       <img src="/titulo.svg" alt="Logo Pintu2" className="logo" />
-      <div className="container">
-      <div className="title-container">
-          <h2 className="left-title finger-paint">Unirse a partida</h2>
-          <h2 className="right-title finger-paint">Crear partida</h2>
-          </div>
       </div>
+      <div className="flex flex-col gap-4 mt-4">
+          <button className="btn_d">Unirse a partida</button>
+          <button className="btn_u">Crear partida</button>
       </div>
       </div>
   );
