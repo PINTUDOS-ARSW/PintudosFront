6d97d343498b55ef7eb8aa562a5ad44e4e9519d0
Juan <112003348+juanescan@users.noreply.github.com>
2025-04-04 01:38:01 -0500

    canvas funcionando todo

--- PintudosFront/src/components/Canvas/Canvas.tsx
+++ PintudosFront/src/components/Canvas/Canvas.tsx
@@ -2,6 +2,7 @@ import React, { use } from 'react';
 import { useEffect, useRef } from 'react';
 function Canvas(){
     const canvasRef = React.useRef<HTMLCanvasElement>(null);
+    const [dibujando, setDibujando] = React.useState(false);
     useEffect(() => {
         const canvas = canvasRef.current;
         if (canvas) {
@@ -10,17 +11,28 @@ function Canvas(){
         }
 },[])
 
+    const evtIniciaDibujo = (evt: React.MouseEvent<HTMLCanvasElement>) => {
+        setDibujando(true);
+        evtDibujaCanvas(evt);
+    };
+
+    const evtTerminaDibujo = (evt: React.MouseEvent<HTMLCanvasElement>) => {
+        setDibujando(false);
+        evtDibujaCanvas(evt);
+    }
+
     interface CanvasMouseEvent extends React.MouseEvent<HTMLCanvasElement> {
         clientX: number;
         clientY: number;
     }
 
     const evtDibujaCanvas = (evt: CanvasMouseEvent) => {
+        if (!dibujando) return;
         const canvas = canvasRef.current;
         if (!canvas) return;
         const ctx = canvas.getContext('2d');
-        const mouseX = evt.clientX;
-        const mouseY = evt.clientY;
+        const mouseX = evt.clientX - canvas.getBoundingClientRect().left;
+        const mouseY = evt.clientY - canvas.getBoundingClientRect().top;
 
         if (ctx) {
             ctx.fillStyle = 'black';
@@ -32,7 +44,9 @@ function Canvas(){
     ref={canvasRef}
     style={{border: '1px solid black'
     , backgroundColor: 'white'}}
-    onMouseMove = {evtDibujaCanvas}>
+    onMouseDown={evtIniciaDibujo}
+    onMouseMove = {evtDibujaCanvas}
+    onMouseUp={evtTerminaDibujo}>
     </canvas>
 )}
 export default Canvas;
